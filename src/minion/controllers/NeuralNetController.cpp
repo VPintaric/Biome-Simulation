016af4550819d0a84c22f0fd63eb1ed079259ef2
Wex <wex@192.168.5.14>
2018-03-02 11:30:46 +0100

    Added missing eigen files...

--- src/minion/controllers/NeuralNetController.cpp
+++ src/minion/controllers/NeuralNetController.cpp
@@ -1,6 +1,29 @@
 #include "minion/controllers/NeuralNetController.h"
+#include "eigen/Dense"
+
+NeuralNetController::NeuralNetController() = default;
+NeuralNetController::~NeuralNetController() = default;
 
 void NeuralNetController::controlMinion(const std::shared_ptr<MinionObject> &m,
                                         const std::shared_ptr<MinionSenses> &senses) {
+    float rotMom, force;
+    rotMom = force = 0.f;
 
+    auto closest = senses->popSenseData();
+    auto sense = senses->popSenseData();
+    if(closest != nullptr){
+        while(sense != nullptr){
+            if(sense->dist < closest->dist){
+                closest = sense;
+            }
+            sense = senses->popSenseData();
+        }
+    }
+
+    if(closest != nullptr){
+
+    }
+
+    m->setControlForce(force);
+    m->setControlRotMoment(rotMom);
 }
