f2c231002767d9d7dcf4f591ca9e3b677348791b
Wex <wex@192.168.5.14>
2018-04-17 22:57:21 +0200

    Added pellet class

---
+++ include/objects/Pellet.h
@@ -0,0 +1,12 @@
+#ifndef BIOME_SIMULATION_PELLET_H
+#define BIOME_SIMULATION_PELLET_H
+
+
+#include <objects/CircleObject.h>
+
+class Pellet : public CircleObject {
+
+};
+
+
+#endif //BIOME_SIMULATION_PELLET_H
---
+++ src/objects/Pellet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wex on 4/17/18.
+//
+
+#include "Pellet.h"
